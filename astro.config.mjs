c2e4f7da6ba43795f2bb1865621cc47a7a00ab04
jourdanmauricio <jourdanmauricio@gmail.com>
2024-02-16 22:52:08 -0300

    Deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
+  site: 'https://jourdanmauricio.github.io',
+  base: '/astro-doc-full-stack',
 });
